2c5270173e575f5ac58114574867b14caae7ff9e
Justin Matsnev <jmatsnev@gmail.com>
2018-01-20 15:30:29 -0500

    added scanner

--- Ding Dong Donor/ViewControllers/DonationPageScannerVC.swift
+++ Ding Dong Donor/ViewControllers/DonationPageScannerVC.swift
@@ -8,80 +8,26 @@
 
 import Foundation
 import UIKit
-import EFQRCode
-import AVFoundation
+import SwiftQRCode
 
 class DonationPageScannerVC : UIViewController {
     
-    var captureSession:AVCaptureSession?
-    var videoPreviewLayer:AVCaptureVideoPreviewLayer?
-    var qrCodeFrameView:UIView?
+    let scanner = QRCode()
+    @IBOutlet weak var scannerImageView : UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        userBackCamera()
+        scanner.prepareScan(view) { (stringValue) -> () in
+            print(stringValue)
+        }
+        scanner.scanFrame = scannerImageView.bounds
     }
     
-    func userBackCamera() {
-        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera], mediaType: AVMediaType.video, position: .back)
-        
-        guard let captureDevice = deviceDiscoverySession.devices.first else {
-            print("Failed to get the camera device")
-            return
-        }
-        do {
-            let input = try AVCaptureDeviceInput(device: captureDevice)
-            // Set the input device on the capture session.
-            captureSession?.addInput(input)
-            let captureMetadataOutput = AVCaptureMetadataOutput()
-            captureSession?.addOutput(captureMetadataOutput)
-            captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
-            captureMetadataOutput.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
-            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
-            videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
-            videoPreviewLayer?.frame = view.layer.bounds
-            view.layer.addSublayer(videoPreviewLayer!)
-            captureSession?.startRunning()
-            
-            qrCodeFrameView = UIView()
-            
-            if let qrCodeFrameView = qrCodeFrameView {
-                qrCodeFrameView.layer.borderColor = UIColor.green.cgColor
-                qrCodeFrameView.layer.borderWidth = 2
-                view.addSubview(qrCodeFrameView)
-                view.bringSubview(toFront: qrCodeFrameView)
-            }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        scanner.startScan()
 
-            
-        } catch {
-            // If any error occurs, simply print it out and don't continue any more.
-            print(error)
-            return
-        }
     }
-    
+   
 }
 
-extension DonationPageScannerVC : AVCaptureMetadataOutputObjectsDelegate {
-
-    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
-        // Check if the metadataObjects array is not nil and it contains at least one object.
-        if metadataObjects.count == 0 {
-            qrCodeFrameView?.frame = CGRect.zero
-            return
-        }
-        
-        // Get the metadata object.
-        let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
-        
-        if metadataObj.type == AVMetadataObject.ObjectType.qr {
-            // If the found metadata is equal to the QR code metadata then update the status label's text and set the bounds
-            let barCodeObject = videoPreviewLayer?.transformedMetadataObject(for: metadataObj)
-            qrCodeFrameView?.frame = barCodeObject!.bounds
-            
-//            if metadataObj.stringValue != nil {
-//                messageLabel.text = metadataObj.stringValue
-//            }
-        }
-    }
-}
