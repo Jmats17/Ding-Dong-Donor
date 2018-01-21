63af6d7feb8b472688f8e9782dbc584866b5fb95
Justin Matsnev <jmatsnev@gmail.com>
2018-01-21 03:09:38 -0500

    backend braintree done

---
+++ Ding Dong Donor/Services/BackendService.swift
@@ -0,0 +1,65 @@
+//
+//  BackendService.swift
+//  Ding Dong Donor
+//
+//  Created by Justin Matsnev on 1/21/18.
+//  Copyright © 2018 Justin Matsnev. All rights reserved.
+//
+
+import BraintreeDropIn
+import Braintree
+import UIKit
+
+struct BackendService {
+    static var clientToken : String?
+    
+    static func fetchClientToken() {
+        // TODO: Switch this URL to your own authenticated API
+        let clientTokenURL = NSURL(string: "https://cannr-app.herokuapp.com/client_token")!
+        let clientTokenRequest = NSMutableURLRequest(url: clientTokenURL as URL)
+        clientTokenRequest.setValue("text/plain", forHTTPHeaderField: "Accept")
+        
+        URLSession.shared.dataTask(with: clientTokenRequest as URLRequest) { (data, response, error) -> Void in
+            // TODO: Handle errors
+            if let data = data {
+                guard let clientToken = String(data: data, encoding: String.Encoding.utf8) else {return}
+                self.clientToken = clientToken
+                // As an example, you may wish to present Drop-in at this point.
+                // Continue to the next section to learn more...
+            }
+            
+            }.resume()
+    }
+    
+    static func showDropIn(clientTokenOrTokenizationKey: String, amt : Int, viewController : UIViewController) {
+        let request =  BTDropInRequest()
+        let dropIn = BTDropInController(authorization: clientTokenOrTokenizationKey, request: request)
+        { (controller, result, error) in
+            if (error != nil) {
+                print("ERROR")
+            } else if (result?.isCancelled == true) {
+                print("CANCELLED")
+            } else if let result = result {
+                // Use the BTDropInResult properties to update your UI
+                result.paymentOptionType = .visa
+                result.paymentOptionType = .applePay
+                BackendService.postNonceToServer(paymentMethodNonce: (result.paymentMethod?.nonce)!, amt: amt)
+                
+            }
+            controller.dismiss(animated: true, completion: nil)
+        }
+        viewController.present(dropIn!, animated: true, completion: nil)
+    }
+    
+    static func postNonceToServer(paymentMethodNonce: String, amt : Int) {
+        // Update URL with your server
+        let paymentURL = URL(string: "https://cannr-app.herokuapp.com/checkout")!
+        var request = URLRequest(url: paymentURL)
+        request.httpBody = "payment_method_nonce=\(paymentMethodNonce)&amt=\(amt)".data(using: String.Encoding.utf8)
+        request.httpMethod = "POST"
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) -> Void in
+            // TODO: Handle success or failure
+            }.resume()
+    }
+}
--- Ding Dong Donor/ViewControllers/PaymentOptionVC.swift
+++ Ding Dong Donor/ViewControllers/PaymentOptionVC.swift
@@ -16,9 +16,7 @@ class PaymentOptionVC : UIViewController {
     @IBOutlet weak var nameLabel : UILabel!
     @IBOutlet weak var creatorLabel : UILabel!
     @IBOutlet weak var amtRaisedLabel : UILabel!
-    
     let barcodeScanner = Scanner()
-    var clientToken : String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,80 +28,23 @@ class PaymentOptionVC : UIViewController {
             })
             
         }
-        fetchClientToken()
+        BackendService.fetchClientToken()
     }
     
-    func showDropIn(clientTokenOrTokenizationKey: String) {
-        let request =  BTDropInRequest()
-        let dropIn = BTDropInController(authorization: clientTokenOrTokenizationKey, request: request)
-        { (controller, result, error) in
-            if (error != nil) {
-                print("ERROR")
-            } else if (result?.isCancelled == true) {
-                print("CANCELLED")
-            } else if let result = result {
-                // Use the BTDropInResult properties to update your UI
-                 result.paymentOptionType = .visa
-                result.paymentOptionType = .applePay
-
-                self.postNonceToServer(paymentMethodNonce: (result.paymentMethod?.nonce)!)
-                
-                // result.paymentIcon
-                // result.paymentDescription
-            }
-            controller.dismiss(animated: true, completion: nil)
-        }
-        self.present(dropIn!, animated: true, completion: nil)
-    }
+    
     
     
     @IBAction func scan(sender : Any) {
         barcodeScanner.presentBarcodeScanner(from: self)
         
     }
-    
-    @IBAction func applyPaySelected(sender : Any) {
 
-    }
-    @IBAction func venmoSelected(sender : Any) {
-        
-    }
     @IBAction func cardSelected(sender : Any) {
       
-        self.showDropIn(clientTokenOrTokenizationKey: self.clientToken ?? "")
+        BackendService.showDropIn(clientTokenOrTokenizationKey: BackendService.clientToken ?? "", amt: 1, viewController: self)
 
     }
     
-    func fetchClientToken() {
-        // TODO: Switch this URL to your own authenticated API
-        let clientTokenURL = NSURL(string: "https://cannr-app.herokuapp.com/client_token")!
-        let clientTokenRequest = NSMutableURLRequest(url: clientTokenURL as URL)
-        clientTokenRequest.setValue("text/plain", forHTTPHeaderField: "Accept")
-        
-        URLSession.shared.dataTask(with: clientTokenRequest as URLRequest) { (data, response, error) -> Void in
-            // TODO: Handle errors
-            if let data = data {
-                guard let clientToken = String(data: data, encoding: String.Encoding.utf8) else {return}
-                self.clientToken = clientToken
-                // As an example, you may wish to present Drop-in at this point.
-                // Continue to the next section to learn more...
-            }
-            
-            }.resume()
-    }
     
-    func postNonceToServer(paymentMethodNonce: String) {
-        // Update URL with your server
-        let paymentURL = URL(string: "https://cannr-app.herokuapp.com/payment-methods")!
-        var request = URLRequest(url: paymentURL)
-        request.httpBody = "payment_method_nonce=\(paymentMethodNonce)".data(using: String.Encoding.utf8)
-        request.httpMethod = "POST"
-        
-        URLSession.shared.dataTask(with: request) { (data, response, error) -> Void in
-            // TODO: Handle success or failure
-            }.resume()
-    }
-    @IBAction func bitcoinSelected(sender : Any) {
-        
-    }
+   
 }
